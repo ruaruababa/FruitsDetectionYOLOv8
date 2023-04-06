2cb87750ff56d26bbe35a4b2b490e523bc291aa4
Yang <dethia1toanlyanh@gmail.com>
2023-04-06 16:45:52 +0700

    refactor

--- services/file_services.py
+++ services/file_services.py
@@ -1,100 +1,54 @@
 import os.path
 import shutil
 
-path = r"C:\Users\yang\OneDrive - Thuyloi University\Images\apple\test\labels"
-# os.path.isfile(
-#     r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\apple_3.jpg"
-# )
-# print(
-#     os.path.isfile(
-#         r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\apple_3.jpg"
-#     )
-# )
+# path = r"C:\Users\yang\OneDrive - Thuyloi University\Images\apple\test\labels"
 
-# for i in range(622, 1000):
-#     if os.path.isfile(
-#         r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\valid\\images\apple_"
-#         + str(i)
-#         + ".txt"
-#     ):
-#         shutil.move(
-#             r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\valid\\images\apple_"
-#             + str(i)
-#             + ".txt",
-#             r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\valid\\labels",
-#         )
-#         print(f"Moving apple_{i}.txt")
-lst = os.listdir(path)  # your directory path
-number_files = len(lst)
-for i in range(0, 1000):
-    hasFile = os.path.isfile(
-        r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-        + str(i)
-        + ".txt"
-    )
-    if hasFile:
-        file_ = open(
-            r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-            + str(i)
-            + ".txt",
-            "r",
-        )
 
-        labels_ = file_.read().splitlines()
-        # labels_i = labels_[i].split(" ")
-        for lebel in labels_:
-            lb = lebel.split(" ")
-            lb = [float(val) if val != "15" else 0 for val in lb]
-            my_list = [str(lb[i]) for i in range(len(lb))]
-            my_list = " ".join(my_list)
-            print(my_list)
-            rf = open(
-                r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-                + str(i)
-                + ".txt",
-                "a",
+def moveFile(currentPath, newPath):
+    for i in range(622, 1000):
+        if os.path.isfile(r"currentPath\apple_" + str(i) + ".txt"):
+            shutil.move(
+                r"currentPath\apple_" + str(i) + ".txt",
+                r"newPath",
             )
-            rf.truncate(0)
-            wf = open(
-                r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-                + str(i)
-                + ".txt",
-                "a",
+            print(f"Moving apple_{i}.txt")
+
+
+def rewrite_class(path_):
+    for i in range(0, 1000):
+        hasFile = os.path.isfile(r"path_\apple_" + str(i) + ".txt")
+        if hasFile:
+            file_ = open(
+                r"path_\apple_" + str(i) + ".txt",
+                "r",
             )
-            wf.write(my_list + "\n")
-        # print(labels_i)
+            labels_ = file_.read().splitlines()
+            for label in labels_:
+                lb = label.split(" ")
+                lb = [float(val) if val != "15" else 0 for val in lb]
+                my_list = [str(lb[i]) for i in range(len(lb))]
+                my_list = " ".join(my_list)
+                rf = open(
+                    r"path_\apple_" + str(i) + ".txt",
+                    "a",
+                )
+                rf.truncate(0)
+                wf = open(
+                    r"path_\apple_" + str(i) + ".txt",
+                    "a",
+                )
+                wf.write(my_list + "\n")
 
-# for i in range(1, 1000):
-#     if os.path.isfile(
-#         r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\images\apple_"
-#         + str(i)
-#         + ".jpg"
-#     ) and os.path.isfile(
-#         r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-#         + str(i)
-#         + ".txt"
-#     ):
-#         print("1")
-#     else:
-#         if os.path.isfile(
-#             r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\images\apple_"
-#             + str(i)
-#             + ".jpg"
-#         ):
-#             os.remove(
-#                 r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\images\apple_"
-#                 + str(i)
-#                 + ".jpg"
-#             )
-#             if os.path.isfile(
-#                 r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-#                 + str(i)
-#                 + ".txt"
-#             ):
-#                 os.remove(
-#                     r"C:\\Users\\yang\\OneDrive - Thuyloi University\\Images\\apple\\test\\labels\apple_"
-#                     + str(i)
-#                     + ".txt"
-#                 )
 
-#             print(f"Removing apple_{i}.jpg and apple_{i}.txt")
+def remove_img_no_labels(path_):
+    for i in range(0, 1000):
+        hasImg = os.path.isfile(r"path_\apple_" + str(i) + ".jpg")
+        hasLabel = os.path.isfile(r"path_\apple_" + str(i) + ".txt")
+        if hasImg and hasLabel:
+            print("TRUE")
+        elif hasImg and not hasLabel:
+            os.remove(r"path_\apple_" + str(i) + ".jpg")
+            print(f"Removing apple_{i}.jpg")
+        elif not hasImg and hasLabel:
+            os.remove(r"path_\apple_" + str(i) + ".txt")
+            print(f"Removing apple_{i}.txt")
